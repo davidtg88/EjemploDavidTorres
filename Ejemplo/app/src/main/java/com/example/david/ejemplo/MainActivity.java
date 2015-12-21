99911a78436481f3242ec3223d46e5a02fcfd7b5
dtorres004 <dtorres004@ikasle.ehu.eus>
2015-12-21 16:43:05 +0100

    version4

--- Ejemplo/app/src/main/java/com/example/david/ejemplo/MainActivity.java
+++ Ejemplo/app/src/main/java/com/example/david/ejemplo/MainActivity.java
@@ -1,25 +1,28 @@
 package com.example.david.ejemplo;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_LOGIN ="es.tta.ejemplo31.login";
     public final static String EXTRA_PASSWD ="es.tta.ejemplo31.passwd";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
     }
 
     @Override
@@ -47,8 +50,33 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,MenuActivity.class);
         EditText editLogin=(EditText)findViewById(R.id.login);
         EditText editPasswd=(EditText)findViewById(R.id.passwd);
-        intent.putExtra(EXTRA_LOGIN,editLogin.getText().toString());
-        intent.putExtra(EXTRA_PASSWD,editPasswd.getText().toString());
-        startActivity(intent);
+        String log="david";
+        String pass="david";
+        if(log.equals(editLogin.getText().toString()) && pass.equals(editPasswd.getText().toString())){
+            intent.putExtra(EXTRA_LOGIN,editLogin.getText().toString());
+            intent.putExtra(EXTRA_PASSWD, editPasswd.getText().toString());
+            Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        }
+       else if(TextUtils.isEmpty(editLogin.getText().toString()) || TextUtils.isEmpty(editPasswd.toString())){
+            Toast.makeText(getApplicationContext(),"Campo Login/Pass en blanco",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this,"Login Incorrecto",Toast.LENGTH_SHORT).show();
+        }
+    }
+    private String[] loadLogin(){
+        SharedPreferences preferences=getPreferences(MODE_PRIVATE);
+        String[] logeo=new String[2];
+        logeo[0]=preferences.getString(EXTRA_LOGIN,null);
+        logeo[1]=preferences.getString(EXTRA_PASSWD,null);
+        return logeo;
+    }
+    private void saveLogin (String usu, String pass){
+        SharedPreferences pref =getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor=pref.edit();
+        editor.putString(EXTRA_LOGIN,usu);
+        editor.putString(EXTRA_PASSWD,pass);
+        editor.commit();
     }
 }
--- Ejemplo/app/src/main/java/com/example/david/ejemplo/TestActivity.java
+++ Ejemplo/app/src/main/java/com/example/david/ejemplo/TestActivity.java
@@ -1,7 +1,9 @@
 package com.example.david.ejemplo;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,10 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URI;
 
 public class TestActivity extends AppCompatActivity implements View.OnClickListener{
     public final static int correct =2;
@@ -53,16 +60,61 @@ public class TestActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.button_send_test).setVisibility(View.VISIBLE);
     }
     public void send (View view){
+        LinearLayout layout=(LinearLayout)findViewById(R.id.test_id_Layout);
+        layout.removeView(findViewById(R.id.button_send_test));
         RadioGroup group=(RadioGroup)findViewById(R.id.test_choices);
+        int seleccionado=group.getCheckedRadioButtonId();
+        RadioButton boton = (RadioButton)findViewById(seleccionado);
         int choices=group.getChildCount();
         for(int i=0;i<choices;i++)
             group.getChildAt(i).setEnabled(false);
-        LinearLayout layout=(LinearLayout)findViewById(R.id.test_id_Layout);
-        layout.removeView(findViewById(R.id.button_send_test));
-
-        group.getChildAt(correct).setBackgroundColor(Color.GREEN);
-
+        int indicesel=group.indexOfChild(boton);
+        if(indicesel==correct){
+            Toast.makeText(this,"Respuesta Correcta",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this,"Respuesta Incorrecta",Toast.LENGTH_SHORT).show();
+            group.getChildAt(correct).setBackgroundColor(Color.GREEN);
+            group.getChildAt(indicesel).setBackgroundColor(Color.RED);
+            Button botonayuda=(Button)findViewById(R.id.button_ayuda_test);
+            botonayuda.setVisibility(View.VISIBLE);
+        }
     }
-
-
+    public void ayuda (View view) {
+        Button boton = (Button) findViewById(R.id.button_ayuda_test);
+        boton.setEnabled(false);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.test_id_Layout);
+        WebView web = new WebView(this);
+        RadioGroup group=(RadioGroup)findViewById(R.id.test_choices);
+        int seleccionado=group.getCheckedRadioButtonId();
+        RadioButton boton2 = (RadioButton)findViewById(seleccionado);
+        int indicesel=group.indexOfChild(boton2);
+        if(indicesel==0){
+            showHtml("<html><body>Has selecionado la opcion 0</body></html>");
+        }
+        else if(indicesel==1){
+            showHtml("http://www.google.com");
+        }
+        else if(indicesel==3){
+            showHtml("http://www.ehu.eus");
+        }
+        else if (indicesel == 4) {
+            showHtml("Esta es una ayuda para saber responde");
+        }
+    }
+    private void showHtml(String advise){
+        if(advise.substring(0,10).contains("://")){
+            Uri uri= Uri.parse(advise);
+            Intent intent=new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+        else{
+            WebView web = new WebView(this);
+            web.loadData(advise, "text/html", null);
+            web.setBackgroundColor(Color.TRANSPARENT);
+            web.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+            LinearLayout layout = (LinearLayout) findViewById(R.id.test_id_Layout);
+            layout.addView(web);
+        }
+    }
 }
